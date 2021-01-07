b050e1d7446e683cdcce2ca75aca1f826c61a07a
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2021-01-07 14:18:34 -0500

    Update FindObjectTitle.py

--- Code/FindObjectTitle.py
+++ Code/FindObjectTitle.py
@@ -10,7 +10,6 @@ def findObjectTitle(source, valueList):
     try:
         h2 = tag.contents[1]
         #h2 is a list, format as: ['\\n', <h2>The Lantern, Ja...span></small>\n</h2>, '\\n']
-        print(h2.contents[0].string)
         value = h2.contents[0].string
     except:
         print("Fail to get record title!")
